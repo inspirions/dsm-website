1760bbcb944f5fa2721563bcd1f300b89049c863
Sajjan Maharjan <sajjmahz@gmail.com>
2025-03-21 18:00:33 +0545

    CU-86erxgjmg-WEB-Add-Font: Add Urbanist font, add mantine UI

---
+++ styles/index.ts
@@ -0,0 +1,42 @@
+import { createTheme } from "@mantine/core";
+
+export const myTheme = createTheme({
+  primaryColor: "dsm-primary",
+  defaultRadius: "md",
+  focusRing: "auto",
+  fontFamily: "Urbanist, sans-serif",
+  headings: { fontFamily: "Urbanist, sans-serif" },
+  components: {
+    ActionIcon: {
+      defaultProps: {
+        variant: "subtle",
+      },
+    },
+    Loader: {
+      defaultProps: {
+        type: "bars",
+      },
+    },
+    Button: {
+      classNames: {
+        root: "outline outline-offset-2 outline-blue-500",
+      },
+    },
+  },
+  colors: {
+    "dsm-primary": [
+      "#ebecff",
+      "#d2d5fe",
+      "#a2a7f9",
+      "#6f77f4",
+      "#454ef0",
+      "#2b34ee",
+      "#023047",
+      "#0f1bd4",
+      "#0617be",
+      "#0012a8",
+    ],
+  },
+});
+// #dadef9 bg
+// #010210 text
