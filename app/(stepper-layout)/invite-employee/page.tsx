94495d13b8d31f63b2bb146100bf41a459837a78
Sajjan Maharjan <sajjan@inspiringlab.com.np>
2025-04-09 18:10:42 +0545

    CU-86et3b0je-Invite-Employee-page-API-integration: integrate API in invite employee page

--- app/(stepper-layout)/invite-employee/page.tsx
+++ app/(stepper-layout)/invite-employee/page.tsx
@@ -2,30 +2,15 @@ import { Flex, Group, Text } from "@mantine/core";
 
 import { DsmButton } from "@/components/DsmButton";
 
-import { InviteEmployeeForm } from "./viewModules/InviteEmployeeForm";
+import { InviteEmployeeFormView } from "./viewModules/InviteEmployeeFormView";
 
 const InviteEmployee = () => {
   return (
-    <Flex h={"100%"} direction={"column"} justify={"space-between"}>
-      <Flex direction={"column"} gap={"md"}>
-        <Text size="xl" fw={700} ta="center">
-          Invite Employees
-        </Text>
-        <InviteEmployeeForm />
-      </Flex>
-      <Group justify="flex-end">
-        <DsmButton hideIcon size="xs" color="blue" variant="transparent">
-          Skip for now
-        </DsmButton>
-        <DsmButton
-          size="xs"
-          color="blue"
-          type="submit"
-          iconProps={{ icon: "user" }}
-        >
-          Send Invites
-        </DsmButton>
-      </Group>
+    <Flex h={"100%"} gap={"md"} direction={"column"}>
+      <Text size="xl" fw={700} ta="center">
+        Invite Employees
+      </Text>
+      <InviteEmployeeFormView />
     </Flex>
   );
 };
