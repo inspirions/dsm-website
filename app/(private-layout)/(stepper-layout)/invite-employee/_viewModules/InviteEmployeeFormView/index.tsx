3eaefcc5e07323366b1237701ddb9d804923b49f
Sajjan Maharjan <sajjan@inspiringlab.com.np>
2025-04-30 15:50:44 +0545

    fix(CU-86eta0dxe-org-list): changed the organization list API

--- app/(private-layout)/(stepper-layout)/invite-employee/_viewModules/InviteEmployeeFormView/index.tsx
+++ app/(private-layout)/(stepper-layout)/invite-employee/_viewModules/InviteEmployeeFormView/index.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { Suspense } from "react";
-import { useRouter } from "next/navigation";
 
 import { handleOrganizationNav } from "@/app/(private-layout)/get-started/actions";
 
 import { useNotification } from "@/hooks/useNotification";
 
 import { commons, DSM_APP_URL } from "@/constants/commons";
-import { routes } from "@/constants/routeConstants";
 
 import { InviteEmployeeForm } from "./InviteEmployeeForm";
 
@@ -16,10 +14,8 @@ import { inviteEmployee } from "../../actions";
 import { InviteEmployeeType } from "../../_schema/InviteEmployee";
 
 const { SUCCESS } = commons;
-const { GET_STARTED } = routes;
 
 export const InviteEmployeeFormView = () => {
-  const router = useRouter();
   const { showNotification, showErrorNotification } = useNotification();
 
   const handleCreateOrganization = async (
@@ -32,8 +28,7 @@ export const InviteEmployeeFormView = () => {
       if (res.code === SUCCESS) {
         await handleOrganizationNav(orgId);
         sessionStorage.clear();
-        window.open(DSM_APP_URL, "_blank");
-        router.push(GET_STARTED);
+        window.location.href = DSM_APP_URL;
       }
 
       showNotification(res.code, res.message);
