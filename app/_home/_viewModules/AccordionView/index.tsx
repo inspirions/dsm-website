22a148cb259f7781c3f338a701c31b1fe8f2b7b5
sameer98441 <sameer@inspiringlab.com.np>
2025-06-13 10:50:56 +0545

    fix(CU-86ettz4ww-fix-update-FAQ-content): update FAQ content

--- app/_home/_viewModules/AccordionView/index.tsx
+++ app/_home/_viewModules/AccordionView/index.tsx
@@ -4,35 +4,7 @@ import { Accordion } from "@mantine/core";
 
 import classes from "./index.module.css";
 import { DsmCustomIcon } from "@/components/DsmCustomIcon";
-
-const FAQS = [
-  {
-    value: "0",
-    title: "What is DailySync.ai?",
-    description:
-      "DailySync.ai is a smart, AI-powered tool designed to automate your daily standups. It listens, transcribes, and summarizes your team's updates, extracting key action items, blockers, and decisions—so you spend less time note-taking and more time getting things done. It's ideal for teams that want better clarity and alignment without the meeting fatigue.",
-  },
-  {
-    value: "1",
-    title: "How does DailySync.ai work?",
-    description: "DailySync.ai works in mysterious ways.",
-  },
-  {
-    value: "2",
-    title: "Who can benefit from using DailySync.ai?",
-    description: "DailySync.ai does not provide benifit to anyone.",
-  },
-  {
-    value: "3",
-    title: "What integrations does DailySync.ai support?",
-    description: "DailySync.ai does not support any integrations.",
-  },
-  {
-    value: "4",
-    title: "Is my data safe with DailySync.ai?",
-    description: "DailySync.ai will sell your data to the highest bidder.",
-  },
-];
+import { FAQS } from "../../_constants";
 
 export const AccordionView = () => {
   const items = FAQS.map((item) => (
