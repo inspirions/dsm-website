2dcade8851ef662e9ef8048beb4720a71f751bd9
Sajjan Maharjan <sajjan@inspiringlab.com.np>
2025-07-11 16:01:38 +0545

    refactor(CU-86eu4n078-orgnaization-list-page-rework): rework the organization list page for users with orgnaization

--- app/(private-layout)/get-started/page.tsx
+++ app/(private-layout)/get-started/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import { Center, Container, Flex, Text } from "@mantine/core";
+import { Box, Center, Container, Flex, Text } from "@mantine/core";
 
 import DsmImage from "@/components/DsmImage";
 import { DsmButton } from "@/components/DsmButton";
@@ -15,7 +15,57 @@ import { GET_STARTED_PAGE } from "@/constants/dataTestId";
 import { getOrganizationList } from "./actions";
 import { OrganizationListView } from "./_viewModules/OrganizationListView";
 
-const { ORGANIZATION } = routes;
+const { LOGIN, ORGANIZATION } = routes;
+
+const CreateOrganization = () => (
+  <Center
+    pb="75px"
+    pt={{ base: 80, md: "xl" }}
+    bg="linear-gradient(to top, #5D3A9B80 0%, #5D3A9B00 42%)"
+  >
+    <DsmContentWrapper>
+      <Flex gap="xl" align="center" justify="center" px={20}>
+        <Flex
+          gap="lg"
+          direction="column"
+          align="flex-start"
+          w={{ base: "100%", md: "60%", lg: "40%" }}
+        >
+          <DsmTitleText text="Get Started on {DailySync}" fz="42px" />
+          <Text size="xl" c="var(--mantine-color-dsm-light-0)">
+            It's an easy way to manage your daily tasks and stand ups for your
+            project and teams. It's faster, better organized, and more secure.
+          </Text>
+          <Flex w="300px">
+            <Link
+              href={ORGANIZATION}
+              style={{ width: "100%", textDecoration: "none" }}
+              data-testid={GET_STARTED_PAGE.CREATE_ORGANIZATION_BTN}
+            >
+              <DsmButton
+                fullWidth
+                size="lg"
+                iconProps={{ icon: "chevronRight", position: "right" }}
+                styles={{ section: { scale: 1.2 } }}
+              >
+                Create an Organization
+              </DsmButton>
+            </Link>
+          </Flex>
+        </Flex>
+        <DsmImage
+          h="auto"
+          w={{ md: "40%", lg: "60%" }}
+          fit="contain"
+          radius="md"
+          src={DSM_IMAGE_URL}
+          display={{ base: "none", md: "block" }}
+          data-testid={GET_STARTED_PAGE.STAND_UP_IMAGE}
+        />
+      </Flex>
+    </DsmContentWrapper>
+  </Center>
+);
 
 const GetStarted = async () => {
   const res = await getOrganizationList();
@@ -45,68 +95,43 @@ const GetStarted = async () => {
     : [];
 
   return (
-    <Flex
-      direction="column"
-      data-testid={GET_STARTED_PAGE.ROOT}
-      c="var(--mantine-color-dsm-light-0)"
-    >
-      <Center
-        pt={{ base: 80, md: "xl" }}
-        pb="75px"
-        bg="linear-gradient(to top, #5D3A9B80 0%, #5D3A9B00 42%)"
-      >
-        <DsmContentWrapper>
-          <Flex gap="xl" align="center" justify="center" px={20}>
-            <Flex
-              gap="lg"
-              w={{ base: "100%", md: "60%", lg: "40%" }}
-              direction="column"
-              align="flex-start"
-            >
-              <DsmTitleText
-                text="Get Started on {DSM}"
-                fz="48px"
-                c="var(--mantine-color-dsm-light-0)"
-              />
-              <Text size="xl" c="var(--mantine-color-dsm-light-0)">
-                It's an easy way to manage your daily tasks and stand ups for
-                your project and teams. It's faster, better organized, and more
-                secure.
+    <Flex direction="column" data-testid={GET_STARTED_PAGE.ROOT}>
+      {!organizations.length ? (
+        <>
+          <CreateOrganization />
+          <Flex
+            gap={"lg"}
+            py={"62px"}
+            align={"center"}
+            direction={"column"}
+            c="var(--mantine-color-gray-4)"
+          >
+            <Flex gap={"sm"} direction={"column"} align={"center"}>
+              <DsmTitleText text="Is your organization already on {DailySync}?" />
+              <Text size="xl" ta={"center"}>
+                We couldn't find any existing organizations for your account
               </Text>
-              <Flex w="300px">
-                <Link
-                  href={ORGANIZATION}
-                  style={{ width: "100%", textDecoration: "none" }}
-                  data-testid={GET_STARTED_PAGE.CREATE_ORGANIZATION_BTN}
-                >
-                  <DsmButton
-                    fullWidth
-                    size="lg"
-                    iconProps={{ icon: "chevronRight", position: "right" }}
-                    styles={{ section: { scale: 1.2 } }}
-                  >
-                    Create an Organization
-                  </DsmButton>
-                </Link>
-              </Flex>
             </Flex>
-            <DsmImage
-              h="auto"
-              w={{ md: "40%", lg: "60%" }}
-              fit="contain"
-              radius="md"
-              src={DSM_IMAGE_URL}
-              display={{ base: "none", md: "block" }}
-              data-testid={GET_STARTED_PAGE.STAND_UP_IMAGE}
-            />
+            <Link
+              href={LOGIN}
+              style={{ textDecoration: "none" }}
+              data-testid={GET_STARTED_PAGE.LOGIN_LINK}
+            >
+              <DsmButton hideIcon size="lg" variant="outline">
+                Switch to another account
+              </DsmButton>
+            </Link>
           </Flex>
-        </DsmContentWrapper>
-      </Center>
-      <Container size="lg">
-        <Center>
-          <OrganizationListView organizations={organizations} />
-        </Center>
-      </Container>
+        </>
+      ) : (
+        <Box bg="linear-gradient(to top, #5D3A9B80 0%, #5D3A9B00 42%)">
+          <Container size="lg">
+            <Center h={"calc(100vh - 82px)"}>
+              <OrganizationListView organizations={organizations} />
+            </Center>
+          </Container>
+        </Box>
+      )}
       <Center pt="xl" bg="var(--mantine-color-dsm-dark-primary-3)">
         <FooterLayout />
       </Center>
