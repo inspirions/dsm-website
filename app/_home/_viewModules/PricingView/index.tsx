dbf561972229e4aebf91d398391c875fcd3ed786
Sajjan Maharjan <sajjan@inspiringlab.com.np>
2025-07-23 16:52:06 +0545

    refactor-pricing: refactor the pricing list

--- app/_home/_viewModules/PricingView/index.tsx
+++ app/_home/_viewModules/PricingView/index.tsx
@@ -14,79 +14,93 @@ const { GROWTH, ADVANCED, ENTERPRISE } = COMMONS;
 export const PricingView = () => {
   return (
     <Box className={classes.pricingFlex}>
-      {PRICING_PLANS.map(({ name, price, period, features, highlight }) => (
-        <Card
-          miw={220}
-          maw={340}
-          radius="lg"
-          key={name}
-          p="32px 24px"
-          shadow={highlight ? "xl" : "md"}
-          w={{ base: "100%", sm: "48%", md: "24%" }}
-          className={
-            name === GROWTH
-              ? `${classes.pricingCard} ${classes.pricingCardSemiHighlight}`
-              : highlight && name === ADVANCED
-                ? `${classes.pricingCard} ${classes.highlightCard}`
-                : classes.pricingCard
-          }
-        >
-          <Flex direction="column" gap="lg" flex={1} w="100%">
-            <Stack gap={8}>
-              <Text fw={700} fz={28}>
-                {name}
-              </Text>
-              <Flex align="end" gap={6}>
-                <Text fw={700} h={62} fz={name === ENTERPRISE ? 32 : 40}>
-                  {price}
+      {PRICING_PLANS.map(
+        ({ name, price, period, features, highlight, users, remark }) => (
+          <Card
+            miw={220}
+            maw={340}
+            radius="lg"
+            key={name}
+            p="32px 24px"
+            shadow={highlight ? "xl" : "md"}
+            w={{ base: "100%", sm: "48%", md: "24%" }}
+            className={
+              name === GROWTH
+                ? `${classes.pricingCard} ${classes.pricingCardSemiHighlight}`
+                : highlight && name === ADVANCED
+                  ? `${classes.pricingCard} ${classes.highlightCard}`
+                  : classes.pricingCard
+            }
+          >
+            <Flex direction="column" gap="md" flex={1} w="100%">
+              <Stack gap={4}>
+                <Text fw={700} fz={28}>
+                  {name}
                 </Text>
-                {period && (
-                  <Text c="gray" fz={18} mb={4}>
-                    {period}
+                <Stack gap={0}>
+                  <Flex align="end" gap={6}>
+                    <Text fw={700} h={62} fz={name === ENTERPRISE ? 32 : 40}>
+                      {price}
+                    </Text>
+                    {period && (
+                      <Text c="gray" fz={18} mb={4}>
+                        {period}
+                      </Text>
+                    )}
+                  </Flex>
+                  {users && (
+                    <Text c="gray" fz={18}>
+                      {users}
+                    </Text>
+                  )}
+                </Stack>
+              </Stack>
+              <Stack gap={8}>
+                {features.map((feature) => (
+                  <Flex key={feature} align="center" gap={8}>
+                    <DsmCustomIcon
+                      icon="circleCheck"
+                      size={20}
+                      style={{ color: "#54e382" }}
+                    />
+                    <Text fz={16} c="#fff">
+                      {feature}
+                    </Text>
+                  </Flex>
+                ))}
+                {remark && (
+                  <Text c="gray" fz={16}>
+                    {remark}
                   </Text>
                 )}
-              </Flex>
-            </Stack>
-            <Stack gap={12}>
-              {features.map((feature) => (
-                <Flex key={feature} align="center" gap={8}>
-                  <DsmCustomIcon
-                    icon="circleCheck"
-                    size={20}
-                    style={{ color: "#54e382" }}
-                  />
-                  <Text fz={16} c="#fff">
-                    {feature}
-                  </Text>
-                </Flex>
-              ))}
-            </Stack>
-          </Flex>
-          <Link href="/sign-up" style={{ textDecoration: "none" }}>
-            <Box
-              className={
-                name === GROWTH
-                  ? `${classes.buttonWrapper} ${classes.semiHighlightButtonWrapper}`
-                  : highlight && name === ADVANCED
-                    ? `${classes.buttonWrapper} ${classes.highlightButtonWrapper}`
-                    : classes.buttonWrapper
-              }
-            >
-              <DsmButton
-                fullWidth
-                hideIcon
-                size="md"
-                bg="transparent"
-                className={`${classes.pricingButton} ${
-                  highlight ? classes.highlightPricingButton : ""
-                }`}
+              </Stack>
+            </Flex>
+            <Link href="/sign-up" style={{ textDecoration: "none" }}>
+              <Box
+                className={
+                  name === GROWTH
+                    ? `${classes.buttonWrapper} ${classes.semiHighlightButtonWrapper}`
+                    : highlight && name === ADVANCED
+                      ? `${classes.buttonWrapper} ${classes.highlightButtonWrapper}`
+                      : classes.buttonWrapper
+                }
               >
-                Start Free Trial
-              </DsmButton>
-            </Box>
-          </Link>
-        </Card>
-      ))}
+                <DsmButton
+                  fullWidth
+                  hideIcon
+                  size="md"
+                  bg="transparent"
+                  className={`${classes.pricingButton} ${
+                    highlight ? classes.highlightPricingButton : ""
+                  }`}
+                >
+                  Start Free Trial
+                </DsmButton>
+              </Box>
+            </Link>
+          </Card>
+        )
+      )}
     </Box>
   );
 };
--- app/_home/_viewModules/StandupStepsView/index.tsx
+++ app/_home/_viewModules/StandupStepsView/index.tsx
@@ -27,10 +27,10 @@ export const StandupStepsView = () => {
       <Flex direction="column" justify="space-around" gap="48px" py="28px">
         {STANDUP_STEPS_VIEW.map((indvStandupStep) => (
           <Flex key={indvStandupStep.title} direction="column" gap="sm">
-            <Text fw={600} lh={1.3} fz={{ base: 14, sm: 20, md: 28 }}>
+            <Text fw={600} lh={1.3} fz={{ base: 18, sm: 22, md: 28 }}>
               {indvStandupStep.title}
             </Text>
-            <Text lh={1.5} fz={{ base: 12, sm: 18, md: 24 }}>
+            <Text lh={1.5} fz={{ base: 14, sm: 18, md: 24 }}>
               {indvStandupStep.description}
             </Text>
           </Flex>
